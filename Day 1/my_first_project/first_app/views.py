5e56fb2a41ec4c3f220beea6a7b941fb78c8c795
rcmehedi15 <hmdmehedi2016@gmail.com>
2022-11-02 18:27:51 +0600

    url back and href practice

--- Day 1/my_first_project/first_app/views.py
+++ Day 1/my_first_project/first_app/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1> This is homepage </h1>")
+    return HttpResponse("<h1> This is Homepage </h1> <a href='contact/'>Contact | <a href='/about/'> About </a> ")
 def contact(request):
-    return HttpResponse("<h1> This is Contact Page </h1>")
+    return HttpResponse("<h1> This is Contact  </h1> <a href='/'>Home Page | <a href='/about/'> About </a>")
 def about(request):
-    return HttpResponse("<h1> This is About Page </h1>")
+    return HttpResponse("<h1> This is About </h1> <a href='/'>Home Page | <a href='/contact/'> Contact Page </a>")
